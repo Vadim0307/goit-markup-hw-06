146df26083b061c796b18814aeb31154ba01872b
Vadim0307 <133796896+Vadim0307@users.noreply.github.com>
2023-07-02 12:00:10 +0300

    add 02.07.2023 12:00

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,8 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-menu-open]"),
-      closeModalBtn: document.querySelector("[data-menu-close]"),
-      modal: document.querySelector("[data-menu]"),
+      openMenuBtn: document.querySelector("[data-menu-open]"),
+      closeMenuBtn: document.querySelector("[data-menu-close]"),
+      menu: document.querySelector("[data-menu]"),
     };
   
     refs.openMenuBtn.addEventListener("click", toggleMenu);
